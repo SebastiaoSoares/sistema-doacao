820573bf94db5015b387eb7a6ba83daa56cad9aa
Icaro-Cavalcante <icarodev06@gmail.com>
2026-03-15 13:22:57 -0300

    Tabelas der

--- src/database/tables.py
+++ src/database/tables.py
@@ -18,15 +18,16 @@ class Tabela():
             Column('id', Integer, primary_key=True, autoincrement=True),
             Column('id_usuario', Integer, ForeignKey('usuarios.id'), nullable=False),
             Column('data_doacao', Date, nullable=False),
-            Column('motivo_recusa', String(255)),
-            Column('status_atual', String(40), nullable=False)
+            Column('descricao', String(255)),
+            Column('status_doacao', String(40), nullable=False)
         )
 
         self.doacoesItem = Table('doacoesItem', self.metadata,
-            Column('id', Integer, ForeignKey('doacoes.id'), primary_key=True),
-            Column('id_item', Integer, ForeignKey('itens.id'), primary_key=True),
+            Column('id', Integer, primary_key=True),
+            Column('id_doacao', Integer, ForeignKey('doacoes.id')),
+            Column('id_item', Integer, ForeignKey('itens.id')),
             Column('quantidade_utilizada', Integer, nullable=False)
-        )
+        ) 
 
             # Usuários
         self.usuarios = Table('usuarios', self.metadata,
@@ -34,10 +35,8 @@ class Tabela():
             Column('nome', String(70)),
             Column('email', String(70), unique=True),
             Column('senha', String(70)),
-            Column('endereco', String(70)),
-            Column('status', String(40)),
-            Column('identificador', String(40), unique=True), # CPF ou CNPJ
-            Column('tipo_perfil', String(40))
+            Column('login', String(70)),
+            Column('data_cadastro', String(40)),
         )
 
         self.pessoasFisica = Table('pessoaFisica', self.metadata,
@@ -55,69 +54,73 @@ class Tabela():
             # Categoria
         self.itensCategoria = Table('itensCategoria', self.metadata,
             Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('nome_categoria', String(50), unique=True, nullable=False)
+            Column('nome_categoria', String(50), unique=True, nullable=False),
+            Column('descricao', String(50), unique=True, nullable=False)
         )
 
             # Itens
         self.itens = Table('itens', self.metadata,
             Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('id_categoria', Integer, ForeignKey('itensCategoria.id')),
+            Column('id_categoria_item', Integer, ForeignKey('itensCategoria.id')),
             Column('descricao', String(255)),
+            Column('nome', String(255)),
             Column('unidade_medida', String(50))
-        )
+        ) 
 
             # Distribuição
         self.distribuicoes = Table('distribuicoes', self.metadata,
             Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('id_pedido', Integer, ForeignKey('pedidosAuxilio.id')),
-            Column('id_doacao', Integer, ForeignKey('doacoes.id')),
-            Column('user_cnpj', String(14), ForeignKey('pessoasJuridica.user_cnpj')),
-            Column('data_entrega', Date, nullable=False),
-            Column('validacao_recebimento', Boolean, default=False)
+            Column('id_pedido_auxilio', Integer, ForeignKey('pedidosAuxilio.id')),
+            Column('status', String(50)),
+            Column('data_distribuicao', Date, nullable=False)
         )
         
         self.distribuicoesItem = Table('distribuicoesItem', self.metadata,
-            Column('id', Integer, ForeignKey('distribuicoes.id'), primary_key=True),
+            Column('id_distribuicao', Integer, ForeignKey('distribuicoes.id'), primary_key=True),
             Column('id_item', Integer, ForeignKey('itens.id'), primary_key=True),
-            Column('user_cnpj', String(14), ForeignKey('pessoasJuridica.user_cnpj')),
             Column('quantidade_utlizada', Integer, nullable=False)
         )
 
         self.rastreios = Table('rastreios', self.metadata,
             Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('id_doacao', Integer, ForeignKey('doacoes.id'), nullable=False),
-            Column('data_hora', Date, nullable=False),
-            Column('etapa', String(100)),
-            Column('localizacao', String(100))
+            Column('id_doacao_item', Integer, ForeignKey('doacoesItem.id'), nullable=False),
+            Column('data_movimentacao', Date, nullable=False),
+            Column('tipo_movimentação', String(100), nullable=False),
+            Column('localizacao', String(100)) 
         )   
            
             # Pedido de auxílio
         self.pedidosAuxilio = Table('pedidosAuxilio', self.metadata,
             Column('id', Integer, primary_key=True, autoincrement=True),
             Column('id_usuario', Integer, ForeignKey('usuarios.id'), nullable=False),
-            Column('flag_fraude', Boolean, default=False),
-            Column('status', String(40), nullable=False),
             Column('justificativa', String(500), nullable=False),
-            Column('data_pedido', Date, nullable=False)
+            Column('data_pedido', Date, nullable=False),
+            Column('status', String(40), nullable=False)
         )
 
             # Voluntariado
         self.vagasVoluntario = Table('vagasVoluntario', self.metadata,
             Column('id', Integer, primary_key=True, autoincrement=True),
             Column('id_usuario', Integer, ForeignKey('usuarios.id'), nullable=False),
-            Column('data_evento', Date, nullable=False),
-            Column('carga_horaria', String(50)),
             Column('titulo', String(100), nullable=False),
-            Column('descricao', String(500), nullable=False)
+            Column('descricao', String(500), nullable=False),
+            Column('data_inicio', Date, nullable=False),
+            Column('data_fim', Date, nullable=False),
+            Column('carga_horaria', String(50)),
+            Column('quantidade_vagas', Integer),
         )
         
         self.inscricoes = Table('inscricoes', self.metadata,
             Column('id', Integer, primary_key=True, autoincrement=True),
             Column('id_vaga', Integer, ForeignKey('vagasVoluntario.id'), nullable=False),
             Column('id_usuario', Integer, ForeignKey('usuarios.id'), nullable=False),
-            Column('data_inscricao', Date, nullable=False),
             Column('status', String(40), nullable=False),
-            Column('checkin_presenca', Boolean, default=False)
+            Column('data_inscricao', Date, nullable=False)
+        )
+
+        self.beneficiarios = Table('beneficiarios', self.metadata,
+            Column('id_usuario', Integer, ForeignKey('usuarios.id'), nullable=False),
+            Column('data_cadastro_beneficiario', Date, nullable=False)
         )
 
         
